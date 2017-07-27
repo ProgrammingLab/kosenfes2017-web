b4849e667b674e0d549eba6fd08704008e4a9dec
Masaki Hashimura <prolab@MacBook-Pro.local>
2017-07-27 17:03:36 +0900

    地図の切り替えができるようになった

--- javascript/route_search.js
+++ javascript/route_search.js
@@ -5,6 +5,7 @@ function initMap() {
     var directionsService2 = new google.maps.DirectionsService;
 	var directionsDisplay3 = new google.maps.DirectionsRenderer;
     var directionsService3 = new google.maps.DirectionsService;
+	var maps = document.getElementsByName("map");
     var map = new google.maps.Map(document.getElementById('map'), {
       zoom: 7
     });
@@ -30,11 +31,13 @@ function initMap() {
     calculateAndDisplayRoute(directionsService, directionsDisplay,"JR久留米駅");
 	directionsDisplay2.setMap(map2);
 	calculateAndDisplayRoute(directionsService2, directionsDisplay2,"西鉄久留米駅");
+	//maps[1].style.display="none";
 	directionsDisplay3.setMap(map3);
 	calculateAndDisplayRoute(directionsService3, directionsDisplay3,"西鉄宮の陣駅");
+	//maps[2].style.display="none";
     }
 
-    function calculateAndDisplayRoute(directionsService, directionsDisplay,result) {
+function calculateAndDisplayRoute(directionsService, directionsDisplay,result) {
     var start = result;
     var end = "久留米高専";
     directionsService.route({
@@ -48,4 +51,25 @@ function initMap() {
         window.alert('Directions request failed due to ' + status);
       }
     });
+}
+
+function DisplayChange(){
+	var radios = document.getElementsByName("grp1");
+ 	var map = document.getElementsByName("map");
+    //取得したラジオボタンオブジェクトから選択されたものを探し出す
+	for(var i=0;i<map.length;i++){
+		map[i].style.display = "none";
+	}
+  	for(var i=0; i<radios.length; i++){
+    	if (radios[i].checked) {
+      		//選択されたラジオボタンのvalue値を取得する
+			map[i].style.display = "block";
+      		break;
+    	}
+  	}
+
+
+
+
+
 }
