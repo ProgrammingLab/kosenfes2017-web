b80d44608512479d32da9303d6cc2852d14433ad
Masaki Hashimura <prolab@MacBook-Pro.local>
2017-07-28 14:49:06 +0900

    昨日の記憶が無いので変更箇所がわからない

--- map/javascript/route_search.js
+++ map/javascript/route_search.js
@@ -13,28 +13,15 @@ function initMap() {
 	      zoom: 7
     });
 	var map3 = new google.maps.Map(document.getElementById('map3'), {
-      zoom: 7
+      zoom: 15
     });
 
     directionsDisplay.setMap(map);
-	//var radios = document.getElementsByName("grp1");
- 
-    //取得したラジオボタンオブジェクトから選択されたものを探し出す
-  /*	var result;
-  	for(var i=0; i<radios.length; i++){
-    	if (radios[i].checked) {
-      		//選択されたラジオボタンのvalue値を取得する
-      		result = radios[i].value;
-      		break;
-    	}
-  	}*/
     calculateAndDisplayRoute(directionsService, directionsDisplay,"JR久留米駅");
 	directionsDisplay2.setMap(map2);
 	calculateAndDisplayRoute(directionsService2, directionsDisplay2,"西鉄久留米駅");
-	//maps[1].style.display="none";
 	directionsDisplay3.setMap(map3);
 	calculateAndDisplayRoute(directionsService3, directionsDisplay3,"西鉄宮の陣駅");
-	//maps[2].style.display="none";
     }
 
 function calculateAndDisplayRoute(directionsService, directionsDisplay,result) {
